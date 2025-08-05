956130ee8259fbd44c2f59420c33f45d8fc324ad
anand-106 <anands2003106@gmail.com>
2025-08-05 20:04:00 +0530

    addedd agent to transcription

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,8 @@ import webrtcvad
 import audioop
 from collections import deque
 from stt import speech_to_text
+from rag import resume_rag_chain,resume_rag_embed
+from agent import interview_graph
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning, module="webrtcvad")
 
@@ -14,7 +16,7 @@ vad =  webrtcvad.Vad(2)
 SAMPLE_RATE = 16000
 FRAME_DURATION = 20
 FRAME_SIZE = int(SAMPLE_RATE*FRAME_DURATION/1000)*2
-SCILENCE_LIMIT = 50
+SCILENCE_LIMIT = 100
 
 
 
@@ -25,57 +27,120 @@ def is_speech(frame: bytes)->bool:
         return False
     return vad.is_speech(frame,SAMPLE_RATE)
 
+async def listen_for_speech(websocket:WebSocket):
+    sclience_window = deque(maxlen=SCILENCE_LIMIT)
+    buffer =b""
+    speech_chunks=0
+    sclience_window.clear()
+    
+    print("Talk Now")
+    
+    while True:
+        chunk = await websocket.receive_bytes()
+    
+
+        # print(f"Received {len(chunk)} bytes of audio.")
+        
+        buffer += chunk
+        
+        for i in range(0,len(chunk),FRAME_SIZE):
+            frame = chunk[i:i+FRAME_SIZE]
+            
+            if len(frame)<FRAME_SIZE:
+                continue
+            
+            speech = is_speech(frame)
+            
+            sclience_window.append(speech)
+            
+            if speech:
+                speech_chunks+=1
+                if speech_chunks ==1:
+                    print("Speech Started")
+            
+            # print("speaking" if speech else "silence")
+            
+            if len(sclience_window) == SCILENCE_LIMIT and not any(sclience_window) and speech_chunks>25:
+                print("end of speech")
+                return buffer
+    
+
 
 @app.websocket("/ws/audio")
 async def audio_ws(websocket:WebSocket):
     
+    print("Trying to accept websocket...")
+    
     await websocket.accept()
     print("websocket Connected")
     
-    buffer = b""
-    sclience_window = deque(maxlen=SCILENCE_LIMIT)
+    retriever = resume_rag_embed(
+        r"C:\Users\gamin\Documents\projects\Mock_Interview_agents_using_Langchain\backend\assets\Anand-S-Resume.pdf"
+    )
     
-    speech_chunks = 0
+    graph = interview_graph()
+
+    state = {
+        "messages": [],
+        "turns": 0,
+        "last_msg": "",
+        "retriever": retriever,
+    }
+
+    stream = graph.stream(state) 
+    
+    buffer = b""
+    
+    
+    
+    
+    # step = next(stream)
+    # current_node, current_state = list(step.items())[0]
+    # state = current_state
+    
+    # if "messages" not in state or not state["messages"]:
+    #     print("\n⚠️ No message returned. Skipping.")
+    
+
+    # last_msg = state["messages"][-1]["text"]
+    # if current_node in {"HR", "TECH", "MANAGER","start","END"}:
+    #     print(f"\n👤 {current_node} says: {last_msg}")
     
     while True:
         try:
-            chunk = await websocket.receive_bytes()
+            step = next(stream)
+            current_node, current_state = list(step.items())[0]
+            state = current_state  
             
-
-            print(f"Received {len(chunk)} bytes of audio.")
+            if "messages" not in state or not state["messages"]:
+                print("\n⚠️ No message returned. Skipping.")
+                continue
             
-            buffer += chunk
+            last_msg = state["messages"][-1]["text"]
             
-            for i in range(0,len(chunk),FRAME_SIZE):
-                frame = chunk[i:i+FRAME_SIZE]
+            if current_node in {"HR", "TECH", "MANAGER","start","END"}:
+                print(f"\n👤 {current_node} says: {last_msg}")
                 
-                if len(frame)<FRAME_SIZE:
-                    continue
+                if state.get("next_interviewer") == "CLOSED":
+                    break
                 
-                speech = is_speech(frame)
-                
-                sclience_window.append(speech)
-                
-                if speech:
-                    speech_chunks+=1
-                
-                print("speaking" if speech else "silence")
-                
-                if len(sclience_window) == SCILENCE_LIMIT and not any(sclience_window) and speech_chunks>10:
-                    print("end of speech")
+                if current_node != "END":
+                    buffer = await listen_for_speech(websocket=websocket)
+                        
                     print("processing Audio")
                     
                     transcription = speech_to_text(buffer)
                     
-                    print(transcription)
-                    
-                    buffer =b""
-                    sclience_window.clear()
-                    
-                    return
+                    print(f"Me: {transcription}")
                     
                     
-        
+
+                    # Only take user input after interviewer nodes
+                    
+                        
+                    state["messages"].append({"role": "candidate", "text": transcription})
+                    state["last_msg"] = transcription
+                   
         except Exception as e:
             print(e)
             break
